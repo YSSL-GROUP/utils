d40afb19cea4eeaf3932e93f9897c89869c8892e
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-07 11:13:44 +0800

    insert: selectFiles

---
+++ src/file/selectFiles.ts
@@ -0,0 +1,21 @@
+/**
+ * 调用浏览器文件选择
+ * @param option - input标签属性配置项
+ * @returns 选择的文件集合
+ */
+export function selectFiles(option: HTMLInputElement) {
+  return new Promise<HTMLInputElement["files"]>((resolve, reject) => {
+    const input = document.createElement("input");
+    input.type = "file";
+    for (const key in option) {
+      input[key] = option[key];
+    }
+    input.addEventListener("change", () => {
+      resolve(input.files);
+    });
+    input.addEventListener("error", (error) => {
+      reject(error);
+    });
+    input.click();
+  });
+}
