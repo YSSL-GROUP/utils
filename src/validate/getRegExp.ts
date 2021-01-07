83b9a55bd79981cb590cae73b32547144141dbb9
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-07 10:50:31 +0800

    insert: getRegExp

---
+++ src/validate/getRegExp.ts
@@ -0,0 +1,10 @@
+import RegExp, { RegExpType } from "./RegExp";
+
+/**
+ * 获取正则表达式
+ * @param type - 正则类型
+ * @returns 指定正则表达式或所有正则表达式集合
+ */
+export default function getRegExp(type?: RegExpType) {
+  return type ? RegExp : RegExp[type];
+}
