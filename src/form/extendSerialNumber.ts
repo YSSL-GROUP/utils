a2d54baaee948a99141881a0051339b9bf81751d
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-07 14:46:53 +0800

    update: extendSerialNumber example

--- src/form/extendSerialNumber.ts
+++ src/form/extendSerialNumber.ts
@@ -4,6 +4,12 @@
  * @param pageSize - 每页条数
  * @param list - 数据列表
  * @returns 添加序号后的数据列表 `key=$serialNumber`
+ * @example
+ * ```ts
+ * const list = [{name: '小明'}, {name: '小红'}]
+ * extendSerialNumber(1, 20, list) // -> [{$serialNumber: 1, name: '小明'}, {$serialNumber: 2, name: '小红'}]
+ * extendSerialNumber(2, 20, list) // -> [{$serialNumber: 11, name: '小明'}, {$serialNumber: 12, name: '小红'}]
+ * ```
  */
 export function extendSerialNumber<T>(page: number, pageSize: number, list: T[]) {
   return list.map((item, idx) => {
