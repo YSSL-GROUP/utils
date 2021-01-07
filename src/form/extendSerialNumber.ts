8c22dbbbbf492afa4a7aeb09e7c020f745bd0700
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-07 11:42:32 +0800

    add: extendSerialNumber

---
+++ src/form/extendSerialNumber.ts
@@ -0,0 +1,13 @@
+/**
+ * 为列表扩展序号字段
+ * @param page - 页码
+ * @param pageSize - 每页条数
+ * @param list - 数据列表
+ * @returns 添加序号后的数据列表 `key=$serialNumber`
+ */
+export function extendSerialNumber<T>(page: number, pageSize: number, list: T[]) {
+  return list.map((item, idx) => {
+    item["$serialNumber"] = idx + 1 + (page - 1) * pageSize;
+    return item;
+  });
+}
