82e96e464a8a47de884d4ca46c9be6be57d2d98b
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-08 17:39:19 +0800

    config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,36 @@
 import { babel } from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { uglify } from "rollup-plugin-uglify";
 // import pkg from "./package.json";
 
-// import { uglify } from "rollup-plugin-uglify";
 const extensions = [".js", ".ts"];
 
 export default {
   input: "src/index.ts",
-  output: {
-    format: "umd",
-  },
-  plugins: [babel({ babelHelpers: "bundled", extensions })],
+  plugins: [
+    nodeResolve({ extensions }),
+    babel({ babelHelpers: "bundled", extensions, presets: ["@babel/preset-env", "@babel/preset-typescript"] }),
+  ],
+  output: [
+    {
+      file: "./dist/rs-utils.umd.js",
+      format: "umd",
+      name: "rsUtils",
+    },
+    {
+      file: "./dist/rs-utils.umd.min.js",
+      format: "umd",
+      name: "rsUtils",
+      plugins: [uglify()],
+    },
+    {
+      file: "./dist/rs-utils.mjs",
+      format: "es",
+    },
+    {
+      file: "./dist/rs-utils.min.mjs",
+      format: "es",
+      plugins: [uglify()],
+    },
+  ],
 };
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,15 @@ export * from "./format";
 export * from "./storage";
 export * from "./style";
 export * from "./validate";
+
+// import convertToBase64 from "./file/convertToBase64";
+// import downloadViaBlob from "./file/downloadViaBlob";
+// import downloadViaUrl from "./file/downloadViaUrl";
+// import selectFiles from "./file/selectFiles";
+
+// export { convertToBase64, downloadViaBlob, downloadViaUrl, selectFiles };
+
+// export * from "./a.ts";
+// export * from "./b.js";
+// export { a };
+// console.log(a);
