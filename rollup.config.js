069ecd0bd23855d8d4a2d69fb81ee407581c1716
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-08 18:02:16 +0800

    config

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,39 @@
 import { babel } from "@rollup/plugin-babel";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { uglify } from "rollup-plugin-uglify";
+import dts from "rollup-plugin-dts";
 // import pkg from "./package.json";
 
 const extensions = [".js", ".ts"];
 
-export default {
-  input: "src/index.ts",
-  plugins: [
-    nodeResolve({ extensions }),
-    babel({ babelHelpers: "bundled", extensions, presets: ["@babel/preset-env", "@babel/preset-typescript"] }),
-  ],
-  output: [
-    {
-      file: "./dist/rs-utils.umd.js",
-      format: "umd",
-      name: "rsUtils",
-    },
-    {
-      file: "./dist/rs-utils.umd.min.js",
-      format: "umd",
-      name: "rsUtils",
-      plugins: [uglify()],
-    },
-    {
-      file: "./dist/rs-utils.mjs",
-      format: "es",
-    },
-  ],
-};
+export default [
+  {
+    input: "src/index.ts",
+    plugins: [
+      nodeResolve({ extensions }),
+      babel({ babelHelpers: "bundled", extensions, presets: ["@babel/preset-env", "@babel/preset-typescript"] }),
+    ],
+    output: [
+      {
+        file: "./dist/rs-utils.umd.js",
+        format: "umd",
+        name: "rsUtils",
+      },
+      {
+        file: "./dist/rs-utils.umd.min.js",
+        format: "umd",
+        name: "rsUtils",
+        plugins: [uglify()],
+      },
+      {
+        file: "./dist/rs-utils.mjs",
+        format: "es",
+      },
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/type.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
