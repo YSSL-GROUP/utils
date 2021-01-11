b6903d1b4b8efd110d6340a5de8cbb3a593b57a8
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-11 17:56:09 +0800

    optimize: format

--- src/format/calculatePercentage.ts
+++ src/format/calculatePercentage.ts
@@ -1,14 +1,27 @@
+import { OUTLIERS } from "@/const";
 /**
  * 计算百分比
  * @param dividend - 被除数
  * @param divisor - 除数
  * @param several - 保留小数点后几位
+ * @param outliers - dividend === null || divisor === null 显示的默认值
  * @returns 百分比
  * @example
  * ```ts
  * calculatePercentage(200, 20, 3) // -> 10.000%
+ * calculatePercentage(null, 20, 3) // -> --
  * ```
  */
-export default function calculatePercentage(dividend: number, divisor: number, several = 2) {
-  return ((dividend / divisor) * 100).toFixed(several) + "%";
+export default function calculatePercentage(
+  dividend: number | string | null,
+  divisor: number | string | null,
+  several = 2,
+  outliers = OUTLIERS,
+) {
+  if (dividend === "null" || divisor === "null") {
+    return outliers;
+  }
+  if (typeof dividend === "number" && typeof divisor === "number") {
+    return ((dividend / divisor) * 100).toFixed(several) + "%";
+  }
 }
