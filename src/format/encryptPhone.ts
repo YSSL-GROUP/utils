9dedec8936ecd285faee27fdb6cd3f945c4f7d30
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-11 17:45:51 +0800

    optimize: encryptPhone

--- src/format/encryptPhone.ts
+++ src/format/encryptPhone.ts
@@ -1,13 +1,15 @@
 /**
  * 手机号加密
  * @param phone - 手机号
+ * @param defaultVal - 手机号为空时默认显示的值
  * @returns 手机号加密后的值
  * @example
  * ```ts
  * encryptPhone(15536435235) // -> 155****5235
+ * encryptPhone(null, '--') // -> --
  * ```
  */
-export default function encryptPhone(phone: string | number | null) {
-  if (!phone) return "";
+export default function encryptPhone(phone: string | number | null, defaultVal = "") {
+  if (!phone) return defaultVal;
   return phone.toString().replace(/(\d{3})\d*(\d{4})/, "$1****$2");
 }
