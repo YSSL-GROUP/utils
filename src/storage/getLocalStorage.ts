ce43e749f9440ec10321079b6f8102f86fc9fa2a
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-12 11:23:39 +0800

    update

--- src/storage/getLocalStorage.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 获取LocalStorage key = `key` 的值
- * @param key - key
- * @returns 经JSON.parse转换后的值
- */
-export default function getLocalStorage<T>(key: string) {
-  const str = localStorage.getItem(key);
-  return str ? (JSON.parse(str) as T) : null;
-}
--- src/storage/setLocalStorage.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 设置LocalStorage key = `key` 的值（值会通过JSON.stringify序列化）
- * @param key - key
- * @returns
- */
-export default function setLocalStorage<T>(key: string, value: T) {
-  const data = JSON.stringify(value);
-  localStorage.setItem(key, data);
-}
