37ef129e4e660bd2752fde91f45071f6c013106c
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-12 15:21:16 +0800

    update

--- src/storage/setSessionStorage.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 设置SessionStorage key = `key` 的值（值会通过JSON.stringify序列化）
- * @param key - key
- * @returns
- */
-export default function setSessionStorage<T>(key: string, value: T) {
-  const data = JSON.stringify(value);
-  sessionStorage.setItem(key, data);
-}
