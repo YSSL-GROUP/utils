c9a74b1eac5c9d97118b2be5bebbaaea419b081a
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-20 17:27:54 +0800

    optimize: ellipsisStr default value

--- src/style/ellipsisStr.ts
+++ src/style/ellipsisStr.ts
@@ -1,15 +1,16 @@
 /**
  * 根据`limit`长度截取`str`加上省略号
+ * @param defaultValue - `str` === null时默认显示的值
  * @param limit - 限制文字长度
  * @param str - 文本
  * @returns 截取并带省略号的字符串
  * @example
  * ```ts
- * ellipsisStr(4, "哈哈哈哈哈哈哈") // -> 哈哈哈哈...
- * ellipsisStr(4, null) // -> ""
+ * ellipsisStr('--', 4, "哈哈哈哈哈哈哈") // -> 哈哈哈哈...
+ * ellipsisStr('--', 4, null) // -> "--"
  * ```
  */
-export function ellipsisStr(limit: number, str: string | null) {
-  if (!str) return "";
+export function ellipsisStr<D>(defaultValue: D, limit: number, str: string | null) {
+  if (!str) return defaultValue;
   return str.length > limit ? str.slice(0, limit) + "..." : str;
 }
