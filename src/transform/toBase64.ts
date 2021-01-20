be434d756e713c131714c26ab596e6d8bb888ede
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-20 17:24:42 +0800

    optimize: toBase64 null

--- src/transform/toBase64.ts
+++ src/transform/toBase64.ts
@@ -1,11 +1,12 @@
+import { isNullable } from "../helper/guard";
 /**
  * 文件或二进制数据转换为base64格式
  * @param data - File | Blob 对象
  * @returns base64字符串
  */
-export function toBase64(data?: File | Blob) {
+export function toBase64(data: File | Blob | null) {
   return new Promise<FileReader["result"]>((resolve, reject) => {
-    if (!data) {
+    if (isNullable(data)) {
       reject(new Error("请传入File/Blob文件"));
     } else {
       const reader = new FileReader();
