09192179478e87040b6a01e1b8ede52c514b6ed9
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-20 17:19:17 +0800

    optimize: maskPhone

--- src/transform/index.ts
+++ src/transform/index.ts
@@ -1,6 +1,6 @@
 export { toFormData } from "./toFormData";
 export { extNo } from "./extNo";
 export { pct } from "./pct";
-export { encryptPhone } from "./encryptPhone";
+export { maskPhone } from "./maskPhone";
 export { toBase64 } from "./toBase64";
 export { toValue } from "./toValue";
--- src/transform/maskPhone.ts
+++ src/transform/maskPhone.ts
@@ -5,11 +5,11 @@
  * @returns 手机号脱敏后的值
  * @example
  * ```ts
- * encryptPhone("--", 15536435235) // -> 155****5235
- * encryptPhone("--", null) // -> --
+ * maskPhone("--", 15536435235) // -> 155****5235
+ * maskPhone("--", null) // -> --
  * ```
  */
-export function encryptPhone<D>(defaultValue: D, phone: string | number | null) {
+export function maskPhone<D>(defaultValue: D, phone: string | number | null) {
   if (phone === null) return defaultValue;
   return phone.toString().replace(/(\d{3})\d*(\d{4})/, "$1****$2");
 }
