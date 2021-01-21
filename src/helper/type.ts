1d9568569c15f6beba8a31cb6a13d2c2e206fde0
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-21 15:12:56 +0800

    type

--- src/helper/type.ts
+++ src/helper/type.ts
@@ -1,8 +1,9 @@
 export type NumberStr = "number";
 export type ObjectStr = "object";
 export type StringStr = "string";
+export type BooleanStr = "boolean";
 export type ArrayStr = "array";
-export type ValueType = NumberStr | ObjectStr | StringStr | ArrayStr;
+export type ValueType = NumberStr | ObjectStr | StringStr | ArrayStr | BooleanStr;
 
 export type AnyObject = { [p in string]: AnyObject | any };
 export type StringObject = { [p in string]: StringObject | string };
