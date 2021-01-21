cb39c112beddb09c9edaf540d832f3ad5a293576
Hanzhaorz <hzoliverdev@gmail.com>
2021-01-21 14:19:21 +0800

    optimize: pct -> toPct

--- src/transform/index.ts
+++ src/transform/index.ts
@@ -1,6 +1,6 @@
 export { toFormData } from "./toFormData";
 export { extNo } from "./extNo";
-export { pct } from "./pct";
+export { toPct } from "./toPct";
 export { maskPhone } from "./maskPhone";
 export { toBase64 } from "./toBase64";
 export { toValue } from "./toValue";
